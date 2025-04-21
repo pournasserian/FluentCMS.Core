3e1138faf6ab14b7f604ed8679d5f4089a7d28d5
Amir Pournasserian <amir.pournasserian@live.com>
2025-04-21 16:57:06 -0400

    Add API result handling classes in FluentCMS.TodoApi
    Introduces the `ApiResult`, `ApiResult<TData>`, `ApiPagedResult<TData>`, and `ApiError` classes to manage API responses. The `ApiResult` class provides a base structure for responses, while the generic versions allow for typed data and pagination support. The `ApiError` class is included to represent error details with appropriate properties and methods.

---
+++ src/FluentCMS.TodoApi/ApiResult.cs
@@ -0,0 +1,83 @@
+﻿namespace FluentCMS.TodoApi;
+
+public class ApiResult
+{
+    public List<ApiError> Errors { get; } = [];
+    public string TraceId { get; set; } = string.Empty;
+    public string SessionId { get; set; } = string.Empty;
+    public string UniqueId { get; set; } = string.Empty;
+    public double Duration { get; set; }
+    public int Status { get; set; }
+    public bool IsSuccess { get; set; }
+
+    public ApiResult()
+    {
+        IsSuccess = true;
+        Status = 200;
+    }
+}
+
+public class ApiResult<TData> : ApiResult
+{
+    public TData? Data { get; set; }
+
+    public ApiResult()
+    {
+    }
+
+    public ApiResult(TData data)
+    {
+        Data = data;
+    }
+}
+
+public class ApiPagedResult<TData> : ApiResult<IEnumerable<TData>>
+{
+    public int PageSize { get; }
+    public int TotalPages { get; }
+    public int PageNumber { get; }
+    public long TotalCount { get; }
+    public bool HasPrevious { get; }
+    public bool HasNext { get; }
+
+    public ApiPagedResult()
+    {
+    }
+
+    public ApiPagedResult(IEnumerable<TData> data, long totalCount, int pageNumber, int pageSize) : base(data)
+    {
+        PageNumber = pageNumber;
+        PageSize = pageSize;
+        TotalCount = totalCount;
+        TotalPages = pageSize > 0 ? (int)Math.Ceiling(totalCount / (double)pageSize) : 0;
+        HasPrevious = PageNumber > 1;
+        HasNext = PageNumber < TotalPages;
+    }
+}
+
+public class ApiError
+{
+    public string Code { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+
+    public ApiError()
+    {
+    }
+
+    public ApiError(string code)
+    {
+        Code = code;
+    }
+
+    public ApiError(string code, string description)
+    {
+        Code = code;
+        Description = description;
+    }
+
+    public override string ToString()
+    {
+        return $"{Code}-{Description}";
+    }
+}
+
