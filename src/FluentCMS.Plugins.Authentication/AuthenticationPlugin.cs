3712f1999c4866a94d5f8f1c402badb29c429398
Amir Pournasserian <amir.pournasserian@live.com>
2025-04-27 10:52:35 -0400

    Refactor global usings and JWT authentication setup
    Updated `_GlobalUsings.cs` to streamline namespaces by removing unnecessary directives and adding required ones for options and token-based authentication.
    
    In `AuthenticationPlugin.cs`, changed the constructor for better structure, updated `ConfigureServices` to use `builder.Configuration`, and refactored the JWT authentication setup for improved clarity and maintainability by removing commented-out code and implementing a more concise configuration.

--- src/FluentCMS.Plugins.Authentication/AuthenticationPlugin.cs
+++ src/FluentCMS.Plugins.Authentication/AuthenticationPlugin.cs
@@ -5,14 +5,17 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace FluentCMS.Plugins.Authentication;
 
-public class AuthenticationPlugin(IConfiguration configuration) : IPlugin
+public class AuthenticationPlugin : IPlugin
 {
     public void ConfigureServices(IHostApplicationBuilder builder)
     {
-        var jwtSettingsSection = configuration.GetSection("JwtSettings");
+        var jwtSettingsSection = builder.Configuration.GetSection("JwtSettings");
         if (!jwtSettingsSection.Exists())
         {
             throw new InvalidOperationException("JwtSettings section is missing from appsettings.json");
@@ -30,27 +33,27 @@ public class AuthenticationPlugin(IConfiguration configuration) : IPlugin
         {
             options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
             options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            var serviceProvider = builder.Services.BuildServiceProvider();
+            var jwtOptions = serviceProvider.GetRequiredService<IOptions<JwtOptions>>().Value;
+
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = jwtOptions.ValidationParameters.ValidateIssuer,
+                ValidateAudience = jwtOptions.ValidationParameters.ValidateAudience,
+                ValidateLifetime = jwtOptions.ValidationParameters.ValidateLifetime,
+                ValidateIssuerSigningKey = jwtOptions.ValidationParameters.ValidateIssuerSigningKey,
+                RequireExpirationTime = jwtOptions.ValidationParameters.RequireExpirationTime,
+                RequireSignedTokens = jwtOptions.ValidationParameters.RequireSignedTokens,
+                ClockSkew = jwtOptions.ValidationParameters.ClockSkew,
+
+                ValidIssuer = jwtOptions.Issuer,
+                ValidAudience = jwtOptions.Audience,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.Secret))
+            };
         });
-        //.AddJwtBearer(options =>
-        //{
-        //    var serviceProvider = services.BuildServiceProvider();
-        //    var jwtOptions = serviceProvider.GetRequiredService<IOptions<JwtOptions>>().Value;
-
-        //    options.TokenValidationParameters = new TokenValidationParameters
-        //    {
-        //        ValidateIssuer = jwtOptions.ValidationParameters.ValidateIssuer,
-        //        ValidateAudience = jwtOptions.ValidationParameters.ValidateAudience,
-        //        ValidateLifetime = jwtOptions.ValidationParameters.ValidateLifetime,
-        //        ValidateIssuerSigningKey = jwtOptions.ValidationParameters.ValidateIssuerSigningKey,
-        //        RequireExpirationTime = jwtOptions.ValidationParameters.RequireExpirationTime,
-        //        RequireSignedTokens = jwtOptions.ValidationParameters.RequireSignedTokens,
-        //        ClockSkew = jwtOptions.ValidationParameters.ClockSkew,
-
-        //        ValidIssuer = jwtOptions.Issuer,
-        //        ValidAudience = jwtOptions.Audience,
-        //        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.Key))
-        //    };
-        //});
 
     }
 
