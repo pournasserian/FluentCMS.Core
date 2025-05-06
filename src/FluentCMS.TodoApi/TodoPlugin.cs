b8e841e32c19eb515741cf95baeb5486860b9a2c
Amir Pournasserian <amir.pournasserian@live.com>
2025-05-06 17:57:37 -0400

    Remove unused imports and commented code in TodoPlugin
    This commit removes the import statement for
    `Microsoft.EntityFrameworkCore` and deletes a
    commented-out line that registered a scoped service
    for `IUnitOfWork` in the `TodoPlugin.cs` file.

--- src/FluentCMS.TodoApi/TodoPlugin.cs
+++ src/FluentCMS.TodoApi/TodoPlugin.cs
@@ -3,7 +3,6 @@ using FluentCMS.DataAccess.Abstractions;
 using FluentCMS.DataAccess.EntityFramework;
 using FluentCMS.TodoApi.Services;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -17,7 +16,6 @@ public class TodoPlugin : IPlugin
         builder.Services.AddScoped<IApplicationUnitOfWork, ApplicationUnitOfWork>();
         builder.Services.AddScoped<ITodoService, TodoService>();
         builder.Services.AddScoped<ITodoRepository, TodoRepository>();
-        //builder.Services.AddScoped<IUnitOfWork, UnitOfWork<ApplicationDbContext>>();
         builder.Services.AddCoreDbContext<ApplicationDbContext>();
     }
 
